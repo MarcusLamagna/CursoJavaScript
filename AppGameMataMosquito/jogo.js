a60ec1f4d43f7b01377b7f433c66e098500da943
MLamagna <marcus@mlamagna.com.br>
2023-10-01 22:09:53 -0300

    Atualizando random e alterando tamanho do mosquito

--- AppGameMataMosquito/jogo.js
+++ AppGameMataMosquito/jogo.js
@@ -25,10 +25,25 @@ console.log(posicaoX, posicaoY)
 //Criar elemento HTML
 var mosquito = document.createElement('img')
 mosquito.src = 'imagens/mosquito.png'
-mosquito.className = 'mosquito1'
+mosquito.className = tamanhoAleatorio()
 mosquito.style.left = posicaoX + 'px'
 mosquito.style.top = posicaoY + 'px'
 mosquito.style.position = 'absolute'
 
 document.body.appendChild(mosquito)
+}
+
+//Crinando uma função aleatória
+function tamanhoAleatorio() {
+	var classe = Math.floor(Math.random() * 3) // Resultado de zero e próximo de 3
+
+	//Tomada de decisão
+	switch(classe) {
+		case 0:
+			return 'mosquito1'
+		case 1:
+			return 'mosquito2'
+		case 2:	
+			return 'mosquito3'
+	}
 }
